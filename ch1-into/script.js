ef56caac8918cebdb520fe5709d5b387eb615333
anbiya eid <you@example.com>
2026-07-03 22:31:46 -0700

    first commit

---
+++ ch1-into/script.js
@@ -0,0 +1,3 @@
+console. log("hello world")
+console. log("football")
+console. log("tennis")
---
+++ ch2-variables-datatypes/script.js
@@ -0,0 +1,24 @@
+var town = "london" // string
+// console. log (town)
+
+
+// var firstName = "ahmed" // string
+// console. log (firstName)
+
+// var lastName = "ciid"// string
+// console. log (lastName)
+
+var age = 25 // number
+// console. log (age)
+
+// var isStudent = true; // boolean
+// console. log(isStudent)
+
+// console.log(age + 5)
+// console.log(firstName * lastName)
+
+// var theNameOfTheManWhoDrivesTheRedCar = "ali"
+
+console.log(typeof age)
+
+console.log(typeof town)
